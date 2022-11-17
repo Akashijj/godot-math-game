6402a11e3491d2bece0a3c92b83e698ffd00bcb1
daniloflorenzano <daniloflorenzano1@outlook.com>
2022-11-16 21:00:14 -0300

    Iinicia funcionalidade de spawn com desafios aleatorios

--- Scripts/Enemie.gd
+++ Scripts/Enemie.gd
@@ -1,30 +1,53 @@
 extends KinematicBody2D
 
 signal damage(amount)
-
 var health: int  = 1
-var challenge: int = 2*4
 var hitted: bool = false
 var velocity = Vector2(-100, 0)
 onready var timer := $Timer as Timer
 var start_timer = false
 
+#############################
+# Dinâmica de desafios
+# para cada inimigo gerado, um dos desafios deve ser atribuido a ele
+#############################
+var easy_challenges = {
+	"3 x 18": "54",
+	"45 - 27": "18",
+	"66 / 6": "11",
+	"39 + 29": "68"
+}
+
+var rng = RandomNumberGenerator.new()
+var n = rng.randomize()
+
+var my_random_number = rng.randf_range(0, easy_challenges.size())	
+
+var challenge = easy_challenges.keys()[my_random_number]
+var answer = easy_challenges.values()[my_random_number]
+
 
 func _ready() -> void:
+	get_node("Label").text = challenge
+	
 	if start_timer:
 		timer.start()
+	
 
-func _on_Node2D_attack_enemie(answer) -> void:
-	if int(answer) == challenge:
+
+func _on_Node2D_attack_enemie(input_answer) -> void:
+	#print(input_answer)
+	
+	if input_answer == answer:
 		print('Resposta correta')
 		# TODO: criar funcao _throw_arrow() que verifique a colisao da flecha
 		# com o inimigo e execute o codigo abaixo
 		hitted = true
 		health -= 1
-		yield(get_tree().create_timer(0.4), "timeout")
+		#yield(get_tree().create_timer(0.4), "timeout")
 		hitted = false
 		if health < 1:
-			queue_free()
+			get_parent().remove_child(self)
 
 
 func _physics_process(delta: float) -> void:
@@ -49,3 +72,4 @@ func _set_animation() -> void:
 func _on_Timer_timeout() -> void:
 	if $ray_wall.is_colliding():
 		emit_signal("damage", 10)
+
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -4,5 +4,20 @@ signal attack_enemie(answer)
 
 
 func _on_input_text_entered(new_text: String) -> void:
-	#print(new_text)
 	emit_signal("attack_enemie", new_text)
+
+
+#############################
+# Spawn de mobs
+#############################
+onready var easy_enemy = preload("res://Scenes/Enemie.tscn")
+onready var spawner := $spawnpoint as Position2D
+
+func newEnemy(typeOfEnemy):
+	var newEnemy = typeOfEnemy.instance()
+	get_parent().add_child(newEnemy)
+	newEnemy.position = spawner.position
+
+func _on_Timer_timeout():
+	newEnemy(easy_enemy)
+
